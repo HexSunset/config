e93d7a42252084c6c215b92732164e3ac9339996
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-08-19 23:05:24 +0300

    Removed automatic clippy

--- nvim/lua/lspconf.lua
+++ nvim/lua/lspconf.lua
@@ -8,13 +8,19 @@ rt.setup({
         }
     },
     server = {
-        on_attach = function(_, bufnr)
-        -- Hover actions
-        vim.keymap.set("n", "<leader>d", rt.hover_actions.hover_actions, { buffer = bufnr })
-        -- Code action groups
-        vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
+        settings = {
+            on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<leader>d", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+        ["rust-analyzer"] = {
+            checkOnSave = {
+                command = "clippy"
+            },
+        },
     },
-})
+}})
 
 rt.inlay_hints.disable()
