f89f00a754f3b100e2a0108521d15b36505beb9a
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-10-17 10:02:35 +0300

    Updated function call

--- nvim/lua/cmp-setup.lua
+++ nvim/lua/cmp-setup.lua
@@ -2,7 +2,7 @@
 local cmp = require'cmp'
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
@@ -100,7 +100,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig')['rust_analyzer'].setup {
   capabilities = capabilities
