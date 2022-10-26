850bfd7d057d25f97025323909ffcaaaaff3e042
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-10-26 12:16:56 +0300

    something usable?

--- emacs/init.el
+++ emacs/init.el
@@ -1,52 +1,65 @@
-;; Glorious EMACS configuration file
-;;
 ;; ███████╗███╗   ███╗ █████╗  ██████╗███████╗
 ;; ██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝
 ;; █████╗  ██╔████╔██║███████║██║     ███████╗
 ;; ██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║
 ;; ███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║
 ;; ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝
-
+                                           
+;; --------------
+;; --- CUSTOM ---
+;; --------------
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
    '("3d2e532b010eeb2f5e09c79f0b3a277bfc268ca91a59cdda7ffd056b868a03bc" default))
- '(package-selected-packages '(evil rust-mode magit gruber-darker-theme auto-complete)))
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(package-selected-packages '(gruber-darker-theme python smex rust-mode))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "ADBO" :family "Fira Code")))))
 
-;; Minimize UI
-(setq inhibit-startup-screen t)
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
+;; ---------------------------
+;; --- INSTALLING PACKAGES ---
+;; ---------------------------
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-;; Line numbers
-(require 'display-line-numbers)
-(setq display-line-numbers 'relative)
- 
-;; Font
-(set-frame-font "Fira Code-16" t)
+(defvar my-packages
+  '(rust-mode
+    python
+    smex
+    gruber-darker-theme)
+  "List of packages to be installed")
 
-;; Theme
-(load-theme 'gruber-darker)
+(defun my-packages-installed-p ()
+  (cl-loop for p in my-packages
+	   when (not (package-installed-p p)) do (cl-return nil)
+	   finally (cl-return t)))
 
-;; Interactively DO things
-(require 'ido)
-(ido-mode t)
+(unless (my-packages-installed-p)
+  (package-refresh-contents)
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
-;; enable evil-mode >:)
-;; (require 'evil)
-;; (evil-mode 1)
 
-;; Package management
-(package-initialize)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+;; --------------------------
+;; --- CONFIGURE PACKAGES ---
+;; --------------------------
+(ido-mode)
+(ido-everywhere 1)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; Old M-x behaviour
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
