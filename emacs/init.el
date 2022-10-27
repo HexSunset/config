0dea73da57839ab0bc9ce2ffacf992d007002dbe
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-10-27 21:57:26 +0300

    * .gitignore: Ignores all emacs subfiles by default

--- emacs/init.el
+++ emacs/init.el
@@ -29,29 +29,12 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "ADBO" :family "Fira Code")))))
 
+
 ;; ---------------------------
 ;; --- INSTALLING PACKAGES ---
 ;; ---------------------------
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
-(defvar my-packages
-  '(rust-mode
-    python
-    smex
-    gruber-darker-theme)
-  "List of packages to be installed")
-
-(defun my-packages-installed-p ()
-  (cl-loop for p in my-packages
-	   when (not (package-installed-p p)) do (cl-return nil)
-	   finally (cl-return t)))
-
-(unless (my-packages-installed-p)
-  (package-refresh-contents)
-  (dolist (p my-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
 
 
 ;; --------------------------
@@ -60,10 +43,12 @@
 (ido-mode)
 (ido-everywhere 1)
 
+;; Smex is ido for M-x
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; Old M-x behaviour
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 
 ;; ----------------------
 ;; --- OTHER SETTINGS ---
