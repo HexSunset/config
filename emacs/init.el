064df91d1e888bffdfbe56487d0270339b508cef
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2022-12-02 08:43:51 +0200

    dynamic spaces emacs

--- emacs/init.el
+++ emacs/init.el
@@ -109,7 +109,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(smex rust-mode magit gruvbox-theme)))
+ '(package-selected-packages '(dynamic-spaces smex rust-mode magit gruvbox-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
