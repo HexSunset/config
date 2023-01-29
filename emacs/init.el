31a5993a58c4554893d1c923e329e7bf38784e35
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-01-29 22:15:10 +0200

    [emacs] Removed thing added by customize

--- emacs/init.el
+++ emacs/init.el
@@ -195,11 +195,11 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(rainbow-mode company-quickhelp org-bullets yasnippet which-key company evil-commentary haskell-mode vterm sudo-edit evil-surround ac-html general evil-collection evil-leader systemd markdown-mode helm use-package smex rust-mode popup magit gruvbox-theme evil async))
- '(safe-local-variable-values '((eval when (fboundp 'rainbow-mode) (rainbow-mode 1)))))
+   '(evil-easymotion rainbow-mode company-quickhelp org-bullets yasnippet which-key company evil-commentary haskell-mode vterm sudo-edit evil-surround ac-html general evil-collection evil-leader systemd markdown-mode helm use-package smex rust-mode popup magit gruvbox-theme evil async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
