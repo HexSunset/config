a536ab18828423350f85521ea43059556c77d76e
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-02-04 00:25:39 +0200

    [emacs] cleaning up

--- emacs/init.el
+++ emacs/init.el
@@ -104,9 +104,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(rainbow-mode company-quickhelp org-bullets yasnippet which-key company haskell-mode vterm sudo-edit ac-html systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme async)))
+   '(rainbow-mode org-bullets yasnippet which-key company haskell-mode vterm sudo-edit ac-html systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
