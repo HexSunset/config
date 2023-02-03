a76579a94d1fdf0848feb44c90fea659f9ec8d2d
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-02-03 23:55:09 +0200

    [emacs] idk man

--- emacs/init.el
+++ emacs/init.el
@@ -102,10 +102,11 @@
  ;; If there is more than one, they won't work right.
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(sm-c-mode evil-easymotion rainbow-mode company-quickhelp org-bullets yasnippet which-key company evil-commentary haskell-mode vterm sudo-edit evil-surround ac-html general evil-collection evil-leader systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme evil async)))
+   '(rainbow-mode company-quickhelp org-bullets yasnippet which-key company haskell-mode vterm sudo-edit ac-html systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme async)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#181818" :foreground "#e4e4ef" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 162 :width normal :foundry "UKWN" :family "Iosevka")))))
+(put 'dired-find-alternate-file 'disabled nil)
