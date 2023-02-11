40bf9e22ae77bd74c088f49321608c520ae0b99e
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-02-11 14:18:51 +0200

    [emacs] make compilation buffer scroll with output

--- emacs/init.el
+++ emacs/init.el
@@ -99,6 +99,9 @@
 ;; --- OTHER SETTINGS ---
 ;; ----------------------
 
+;; compilation buffer
+(setq compilation-scroll-output t)
+
 (global-set-key (kbd "M-o") 'other-window)
 
 ;; indentation
@@ -119,7 +122,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(rainbow-mode org-bullets yasnippet which-key company haskell-mode vterm sudo-edit ac-html systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme async)))
+   '(zig-mode rainbow-mode org-bullets yasnippet which-key company haskell-mode vterm sudo-edit ac-html systemd markdown-mode use-package smex rust-mode popup magit gruvbox-theme async)))
 (put 'dired-find-alternate-file 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
