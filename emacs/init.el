67b3144fcf9a3c2f57e09f72961d53e221996917
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-02-24 13:29:28 +0200

    [emacs] functions for working with marks

--- emacs/init.el
+++ emacs/init.el
@@ -37,6 +37,20 @@
       (message "Opening file...")
     (message "Aborting")))
 
+;; mark functions
+(defun push-mark-no-activate ()
+  "Pushes `point' to `mark-ring' and does not activate the region
+   Equivalent to \\[set-mark-command] when \\[transient-mark-mode] is disabled"
+  (interactive)
+  (push-mark (point) t nil)
+  (message "Pushed mark to ring"))
+
+(defun jump-to-mark ()
+  "Jumps to the local mark, respecting the `mark-ring' order.
+  This is the same as using \\[set-mark-command] with the prefix argument."
+  (interactive)
+  (set-mark-command 1))
+
 
 ;; ------------------
 ;; --- APPEARANCE ---
