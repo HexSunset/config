cf5e6ef3cda5dfd36e8a5a540f4a3ddb5f22b221
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-02-24 22:48:29 +0200

    [emacs] goofy silly serif font is here

--- emacs/init.el
+++ emacs/init.el
@@ -82,7 +82,8 @@
 ;; ------------------
 (add-to-list 'custom-theme-load-path "~/.config/emacs/elisp/themes/")
 (load-theme 'gruber-darker t) ;; The t makes emacs not ask before loading
-(set-face-attribute 'default nil :font "Iosevka" :height 160)
+(set-face-attribute 'default nil :font "Iosevka Fixed Slab" :height 160)
+
 ;; Line numbers
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
@@ -214,6 +215,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Iosevka"))))
- '(fixed-pitch-serif ((t (:family "Iosevka"))))
- '(variable-pitch ((t (:family "IBM Plex Sans")))))
+ '(fixed-pitch ((t (:family "Iosevka Fixed Normal"))))
+ '(fixed-pitch-serif ((t (:family "Iosevka Fixed Slab"))))
+ '(variable-pitch ((t (:family "Iosevka Etoile")))))
