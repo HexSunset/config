003d9609d767da7df94885dd1868c8fbb0b92319
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-05-05 19:04:31 +0300

    [emacs] load appearance settings first, reduce flashbang time

--- emacs/init.el
+++ emacs/init.el
@@ -11,8 +11,8 @@
 (add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "themes" user-emacs-directory))
 
+(require 'au-appearance)
 (require 'au-fix-defaults)
 (require 'au-settings)
 (require 'au-packages)
-(require 'au-appearance)
 (require 'au-key-bindings)
