6d63620558e16f8dc84651478b0a039240664d6f
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-06-07 16:36:29 +0300

    [emacs] fix ido switching directories on its own

--- emacs/settings/au-settings.el
+++ emacs/settings/au-settings.el
@@ -3,6 +3,9 @@
 
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; Stop the directory switching IDO likes to do.
+(setq ido-auto-merge-work-directories-length -1)
+
 ;; No more backups
 (setq make-backup-files nil)
 
