e934bc77f6f287e8d0db484af17c027c60562072
Aurora Luhamaa <aurora.luhamaa@protonmail.com>
2023-06-13 21:38:21 +0300

    [emacs] load font on all frames that are created

--- emacs/settings/au-appearance.el
+++ emacs/settings/au-appearance.el
@@ -1,13 +1,15 @@
 ;; -*- lexical-binding: t; -*-
 
-(set-face-attribute 'default nil :font "Iosevka Fixed Slab 16")
-(set-face-attribute 'fixed-pitch nil :font "Iosevka Fixed 16")
+(add-to-list 'default-frame-alist
+	     '(font . "Iosevka Fixed Slab-16"))
 
 (require 'gruber-darker-theme)
 (load-theme 'gruber-darker t)
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
+
+(setq tab-width 4)
 
 
 (provide 'au-appearance)
